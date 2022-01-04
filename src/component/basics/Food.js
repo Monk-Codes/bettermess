1ad5ccef41ad6880e230c22f7c483f84a6ccfaa9
dizzy <84877191+drpratikk@users.noreply.github.com>
2022-01-04 21:14:14 +0530

    final

--- src/component/basics/Food.js
+++ src/component/basics/Food.js
@@ -2,23 +2,36 @@ import React, { useState } from "react";
 import "./Style.css";
 import Menu from "./MenuApi";
 import MenuCard from "./MenuCard";
+import Navbar from "./Navbar";
 
+const uniqueList = [
+    ...new Set(
+        Menu.map((curElem,id) => {
+            <div key={id} />
+            return (curElem.category);
+        })
+    ),"All",
+];
 const Food = () => {
-  const [menuData, setMenuData] = useState(Menu);
-  return (
-    <>
-      <nav className="navbar">
-        <div className="btn-group">
-          <button className="btn-group__item">Breakfast</button>
-          <button className="btn-group__item">Lunch</button>
-          <button className="btn-group__item">Dinner</button>
-          <button className="btn-group__item">Night-Cravings</button>
-          <button className="btn-group__item">All</button>
-        </div>
-      </nav>
-      <MenuCard menuData={menuData} />
-    </>
-  );
+    const [menuData, setMenuData] = useState(Menu);
+    const [menuList] = useState(uniqueList);
+
+    const filterItem = (category) => {
+        if(category==="All"){
+            setMenuData(Menu)
+            return
+        }
+        const updatedList = Menu.filter((curElem) => {
+            return curElem.category === category;
+        });
+        setMenuData(updatedList);
+    };
+    return (
+        <>
+            <Navbar filterItem={filterItem} menuList={menuList} />
+            <MenuCard menuData={menuData} />
+        </>
+    );
 };
 
 export default Food;
--- src/component/basics/MenuCard.js
+++ src/component/basics/MenuCard.js
@@ -1,32 +1,36 @@
 import React from "react";
 
-const menuCard = ({ menuData }) => {
-  return (
-    <>
-      <section className="main-card--container">
-        {menuData.map((curElem) => {
-          return (
-            <>
-              <div className="card-container" key={curElem.id}>
-                <div className="card">
-                  <div className="card-body">
-                    <span className="card-number card-circle subtle">{curElem.id}</span>
-                    <span className="card-author subtle">{curElem.category}</span>
-                    <h2 className="card-title">{curElem.name}</h2>
-                    <span className="card-decription subtle">{curElem.description}</span>
-                    <div className="card-read">Read</div>
-                  </div>
-                  <img src={curElem.image} alt="images" className='card-media' />
-                  <span className="card-tag subtle" >Order Now</span>
-                  <span className="card-tag subtle">{curElem.price}</span>
-                </div>
-              </div>
-            </>
-          );
-        })}
-      </section>
-    </>
-  );
+const MenuCard = ({ menuData }) => {
+    return (
+        <>
+            <section className="main-card--container" >
+                {menuData.map((curElem) => {
+                    const { id, name, category, price, description, image } = curElem;
+                    return (
+                        <>
+                            <div className="card-container"key={curElem.id} >
+                                <div className="card" >
+
+                                    <div className="card-body">
+                                        <span className="card-number card-circle subtle">{id}</span>
+                                        <span className="card-author subtle">{category}</span>
+                                        <h2 className="card-title">{name}</h2>
+                                        <span className="card-decription subtle">{description}</span>
+                                        <div className="card-read">Read</div>
+                                    </div>
+
+                                    <img src={image} alt="images" className="card-media" />
+                                    <span className="card-tag subtle">Order Now</span>
+                                    <span className="card-tag subtle">{price}</span>
+                                </div>
+
+                            </div>
+                        </>
+                    );
+                })}
+            </section>
+        </>
+    );
 };
 
-export default menuCard;
+export default MenuCard;
